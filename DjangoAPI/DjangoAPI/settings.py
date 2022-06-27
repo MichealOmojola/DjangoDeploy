efdd4916ada64eb7a8a3556f9597abe7a764b0ae
MichealOmojola <75433192+OmoMicheal@users.noreply.github.com>
2022-06-27 16:06:29 +0100

    added STATIC_ROOT

--- DjangoAPI/DjangoAPI/settings.py
+++ DjangoAPI/DjangoAPI/settings.py
@@ -32,6 +32,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Application definition
 
 INSTALLED_APPS = [
