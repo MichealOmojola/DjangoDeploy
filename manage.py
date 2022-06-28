ad38f7548c5d2368bc7f95e0896863127d048146
MichealOmojola <75433192+OmoMicheal@users.noreply.github.com>
2022-06-28 10:15:02 +0100

    pushed

--- manage.py
+++ manage.py
@@ -5,38 +5,38 @@ import sys
 # import pickle
 
 
-import pickle
-from tensorflow.keras.models import Model
-from tensorflow.python.keras.layers import deserialize, serialize
-from tensorflow.python.keras.saving import saving_utils
+# import pickle
+# from tensorflow.keras.models import Model
+# from tensorflow.python.keras.layers import deserialize, serialize
+# from tensorflow.python.keras.saving import saving_utils
 
 
-def unpack(model, training_config, weights):
-    restored_model = deserialize(model)
-    if training_config is not None:
-        restored_model.compile(
-            **saving_utils.compile_args_from_training_config(
-                training_config
-            )
-        )
-    restored_model.set_weights(weights)
-    return restored_model
+# def unpack(model, training_config, weights):
+#     restored_model = deserialize(model)
+#     if training_config is not None:
+#         restored_model.compile(
+#             **saving_utils.compile_args_from_training_config(
+#                 training_config
+#             )
+#         )
+#     restored_model.set_weights(weights)
+#     return restored_model
 
-# Hotfix function
-def make_keras_picklable():
+# # Hotfix function
+# def make_keras_picklable():
 
-    def __reduce__(self):
-        model_metadata = saving_utils.model_metadata(self)
-        training_config = model_metadata.get("training_config", None)
-        model = serialize(self)
-        weights = self.get_weights()
-        return (unpack, (model, training_config, weights))
+#     def __reduce__(self):
+#         model_metadata = saving_utils.model_metadata(self)
+#         training_config = model_metadata.get("training_config", None)
+#         model = serialize(self)
+#         weights = self.get_weights()
+#         return (unpack, (model, training_config, weights))
 
-    cls = Model
-    cls.__reduce__ = __reduce__
+#     cls = Model
+#     cls.__reduce__ = __reduce__
 
-# Run the function
-# make_keras_picklable()
+# # Run the function
+# # make_keras_picklable()
 
 
 def main():
@@ -53,5 +53,5 @@ def main():
 
 
 if __name__ == '__main__':
-    make_keras_picklable()
+    # make_keras_picklable()
     main()
