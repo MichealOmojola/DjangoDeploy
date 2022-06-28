4ded6333858d64f851cd14d42ae1329c4cffb1d1
MichealOmojola <75433192+OmoMicheal@users.noreply.github.com>
2022-06-28 11:00:43 +0100

    newly_pushed

--- MyAPI/views.py
+++ MyAPI/views.py
@@ -26,39 +26,6 @@ from keras import backend as K
 
 
 
-import pickle
-from tensorflow.keras.models import Model
-from tensorflow.python.keras.layers import deserialize, serialize
-from tensorflow.python.keras.saving import saving_utils
-
-
-def unpack(model, training_config, weights):
-    restored_model = deserialize(model)
-    if training_config is not None:
-        restored_model.compile(
-            **saving_utils.compile_args_from_training_config(
-                training_config
-            )
-        )
-    restored_model.set_weights(weights)
-    return restored_model
-
-# Hotfix function
-def make_keras_picklable():
-
-    def __reduce__(self):
-        model_metadata = saving_utils.model_metadata(self)
-        training_config = model_metadata.get("training_config", None)
-        model = serialize(self)
-        weights = self.get_weights()
-        return (unpack, (model, training_config, weights))
-
-    cls = Model
-    cls.__reduce__ = __reduce__
-
-
-
-
 
 
 
@@ -109,9 +76,8 @@ def ohevalue(df):
 
 # @api_view(["POST"])
 def approvereject(unit):
+
     
-    # Run the function
-    make_keras_picklable()
 
     try:
         with open(model_address, "rb") as input_file:
