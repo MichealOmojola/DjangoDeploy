2e2a451b8bca565e1f813bf3ba9e152b32cdeb87
MichealOmojola <75433192+OmoMicheal@users.noreply.github.com>
2022-06-28 02:07:55 +0100

    nucie

--- DjangoAPI/DjangoAPI/settings.py
+++ DjangoAPI/DjangoAPI/settings.py
@@ -82,24 +82,24 @@ WSGI_APPLICATION = 'DjangoAPI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'mydb1',
-#         'USER': 'user1',
-#         'PASSWORD': 'qwerty12345',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb1',
+        'USER': 'user1',
+        'PASSWORD': 'qwerty12345',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 
 
