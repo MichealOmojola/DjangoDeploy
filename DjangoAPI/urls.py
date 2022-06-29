f1ece372258eed54ebab10e7c93168f34fb193c0
MichealOmojola <75433192+OmoMicheal@users.noreply.github.com>
2022-06-29 14:10:42 +0100

    bubbjbkj

--- DjangoAPI/urls.py
+++ DjangoAPI/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from django.urls import re_path, include
+from django.urls import path, include
 
 urlpatterns = [
     # path('MyAPI/', include('MyAPI.site.urls')), # for other API names
 
-    re_path('admin/', admin.site.urls),
-    re_path('', include('MyAPI.urls')), # for our current API
+    path('admin/', admin.site.urls),
+    path('', include('MyAPI.urls')), # for our current API
     # path('MyAPI/', include('MyAPI.urls')), # for other API names
     # path('apiname/', MyAPI.site.urls), # for other API names
 ]
