136bcd8fd0766ef018328466da541945b37545d7
MichealOmojola <75433192+OmoMicheal@users.noreply.github.com>
2022-06-30 13:49:56 +0100

    cacadc

--- DjangoAPI/urls.py
+++ DjangoAPI/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     # path('MyAPI/', include('MyAPI.site.urls')), # for other API names
@@ -8,4 +9,6 @@ urlpatterns = [
     path('', include('MyAPI.urls')), # for our current API
     # path('MyAPI/', include('MyAPI.urls')), # for other API names
     # path('apiname/', MyAPI.site.urls), # for other API names
-] 
+]
+urlpatterns += staticfiles_urlpatterns()
+
