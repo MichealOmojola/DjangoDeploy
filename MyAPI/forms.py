2a3fefe5e953cb490f1f82a8f22e926e9fd9dba2
MichealOmojola <75433192+OmoMicheal@users.noreply.github.com>
2022-06-30 16:35:37 +0100

    pushed

--- MyAPI/forms.py
+++ MyAPI/forms.py
@@ -24,20 +24,18 @@ class ApprovalForm(forms.Form):
     ]
     CREDIT_HISTORY_CHOICES = [
         ('0', 0),
-        ('1', 1),
-        ('2', 2),
-        ('3', 3)
+        ('1', 1)
     ]
 
-    firstname = forms.CharField(max_length=150, widget=forms.TextInput(attrs={'placeholder': 'Enter Firstname'}))
-    lastname = forms.CharField(max_length=150, widget=forms.TextInput(attrs={'placeholder': 'Enter Lastname'}))
-    Dependents = forms.IntegerField(widget=forms.TextInput(attrs={'placeholder': 'Enter Number of Dependents'}))
-    ApplicantIncome = forms.IntegerField(widget=forms.TextInput(attrs={'placeholder': 'Enter Monthly Gross Income'}))
-    LoanAmount = forms.IntegerField(widget=forms.TextInput(attrs={'placeholder': 'Enter Loan Amount'}))
-    Loan_Amount_Term = forms.IntegerField(widget=forms.TextInput(attrs={'placeholder': 'Enter Term in Months'}))
-    Credit_History = forms.ChoiceField(choices=CREDIT_HISTORY_CHOICES)
-    Gender = forms.ChoiceField(choices=GENDER_CHOICES)
-    Married = forms.ChoiceField(choices=MARRIED_CHOICES)
-    Education = forms.ChoiceField(choices=GRADUATED_CHOICES)
-    Self_Employed = forms.ChoiceField(choices=SELFEMPLOYED_CHOICES)
-    Property_Area = forms.ChoiceField(choices=PROPERTY_CHOICES)
+    firstname = forms.CharField(label='FIRST NAME', max_length=150, widget=forms.TextInput(attrs={'placeholder': 'Enter Firstname'}))
+    lastname = forms.CharField(label='LAST NAME', max_length=150, widget=forms.TextInput(attrs={'placeholder': 'Enter Lastname'}))
+    Dependents = forms.IntegerField(label='NUMBER OF DEPENDANTS', widget=forms.TextInput(attrs={'placeholder': 'Enter Number of Dependents'}))
+    ApplicantIncome = forms.IntegerField(label="APPLICANT'S INCOME", widget=forms.TextInput(attrs={'placeholder': 'Enter Monthly Gross Income'}))
+    LoanAmount = forms.IntegerField(label="LOAN AMOUNT", widget=forms.TextInput(attrs={'placeholder': 'Enter Loan Amount'}))
+    Loan_Amount_Term = forms.IntegerField(label='LOAN TENOR', widget=forms.TextInput(attrs={'placeholder': 'Enter Loan Tenor'}))
+    Credit_History = forms.ChoiceField(label='CREDIT HISTORY', choices=CREDIT_HISTORY_CHOICES)
+    Gender = forms.ChoiceField(label='GENDER', choices=GENDER_CHOICES)
+    Married = forms.ChoiceField(label='MARITAL STATUS', choices=MARRIED_CHOICES)
+    Education = forms.ChoiceField(label='EDUCATION', choices=GRADUATED_CHOICES)
+    Self_Employed = forms.ChoiceField(label='EMPLOYMENT STATUS', choices=SELFEMPLOYED_CHOICES)
+    Property_Area = forms.ChoiceField(label='RESIDENTIAL AREA', choices=PROPERTY_CHOICES)
